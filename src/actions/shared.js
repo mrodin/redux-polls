5dc1d8eff4828c80c14f0e09fc65e99918b67091
Martin Rodin <rodin.martin@gmail.com>
2018-09-17 08:32:11 +0200

    add LoadingBar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import { getInitialData } from '../utils/api';
 import { receiveUsers } from './users';
 import { receivePolls } from './polls';
 import { setAuthUser } from './authedUser';
+import { showLoading, hideLoading } from 'react-redux-loading';
 
 const AUTHED_ID = 'tylermcginnis';
 
 export function handleInitialData() {
   return (dispatch) => {
+    dispatch(showLoading());
     return getInitialData()
       .then(({ users, polls }) => {
         dispatch(receiveUsers(users));
         dispatch(receivePolls(polls));
         dispatch(setAuthUser(AUTHED_ID));
+        dispatch(hideLoading());
       })
   }
 }
